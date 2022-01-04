b8c55d2015a42c3be302c25ddb069f46c37c374d
beiweixiaowang <wyh2073@gmail.com>
2022-01-04 12:33:14 +0800

    构建bert pipline

--- train_bert.py
+++ train_bert.py
@@ -2,13 +2,9 @@ import pandas as pd
 import numpy as np
 import warnings
 warnings.simplefilter("ignore")
-from scipy.special import softmax
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
-from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
-from tqdm.notebook import tqdm
 from sklearn.metrics import *
 from sklearn.model_selection import *
-import re
 import random
 import torch
 pd.options.display.max_colwidth = 200
@@ -20,18 +16,13 @@ class Args:
     n_fold = 5
     seed = 42
     batch_size = 2
-    learning_rate = 5e-5
+    learning_rate = 4e-5
     max_len = 256
     nums_labels = 25
     epochs = 10
     weight_decay = 0.01
     warmup_ratio = 0.1
     warmup_steps = 0
-
-
-
-
-
 
 
 def seed_all(seed_value):
@@ -60,22 +51,57 @@ def f1_multiclass(labels, preds):
     return f1_score(labels, preds, average='micro')
 
 
-def train_model(args):
+def train_model(args, train, test):
     err = []
     acc = []
     y_pred_tot = []
+    # id_list = test['id'].tolists()
+
+    train_data = train[['text', 'label']]
+    test_data = test[['text']]
+
     fold = StratifiedKFold(n_splits=args.n_fold, shuffle=True, random_state=args.seed)
-    i = 1
     model_args = ClassificationArgs(num_train_epochs=args.epochs,
                                     train_batch_size=args.batch_size,
                                     reprocess_input_data=True,
                                     overwrite_output_dir=True,
                                     warmup_ratio=args.warmup_ratio,
                                     warmup_steps=args.warmup_steps,
-                                    weight_decay=args.weight_decay
-
+                                    weight_decay=args.weight_decay,
+                                    save_model_every_epoch=False,
+                                    save_eval_checkpoints=False
                                     )
+    for train_index, test_index in fold.split(train_data, train_data['label']):
+        train_trn, train_val = train_data.iloc[train_index], train_data.iloc[test_index]
+        model = ClassificationModel(args.model_name, args.model_path, use_cuda=True,
+                                    num_labels=args.nums_labels, args=model_args)
+        model.train_model(train_trn)
+        result, raw_outputs_val, wrong_predictions = model.eval_model(train_val, acc=accuracy_score)
+        print(result)
+        err.append(result['eval_loss'])
+        acc.append(result['acc'])
+        raw_outputs_test = model.predict(test_data['text'].tolist())[1]
+        y_pred_tot.append(raw_outputs_test)
+    print("Mean LogLoss: ", np.mean(err))
+    print("Mean acc:", np.mean(acc))
+    final = pd.DataFrame(np.mean(y_pred_tot, 0))
+    final.insert(0, 'id', test['id'].tolist())
+    print(final.shape)
+    return final
+
+
+def main():
+    Args.model_name = 'roberta'
+    Args.model_path = 'hfl/chinese-roberta-wwm-ext'
+    train = pd.read_csv("input/data/train.csv")
+    test = pd.read_csv("input/data/testa_nolabel.csv")
+    train = process_data(train)
+    test = process_data(test)
+    print(train.shape, test.shape)
+    result = train_model(Args, train, test)
+    print(result.shape)
+    result.to_csv("robert.csv", index=False)
 
 
 if __name__ == '__main__':
-
+    main()
